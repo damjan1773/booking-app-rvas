8fe8c6e64aedc36019a7842183fe3e5fc9b01d20
Micagaa <milosavljevicmilun9@gmail.com>
2026-05-12 23:00:33 +0200

    feat: Accommodation model

--- obj/Debug/net10.0/BookingApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/BookingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+390ef222ac1bc842be63361867f2a664ca4778de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70b9f23b999a06f68b08b5c68ee5c085edfe1906")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
