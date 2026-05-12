3fc19fabddadd869e6be3588f5a732546924fd20
Micagaa <milosavljevicmilun9@gmail.com>
2026-05-13 00:17:26 +0200

    feat: dodat uploads folder sa gitkeep

--- obj/Debug/net10.0/BookingApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/BookingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb8019555b8448fa7eccaf5be707ca16863a9e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e034124cf5e3f196ec1fd9bcdebeb8bbc860599e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
