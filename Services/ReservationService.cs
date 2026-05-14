259044998883495aadeba1d4cc426406b15013e0
Lazar Lukić <lukic.lazar33@gmail.com>
2026-05-14 22:46:18 +0200

    feat: ReservationService - osnovni CRUD

---
+++ Services/ReservationService.cs
@@ -0,0 +1,33 @@
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using BookingApp.Models;
+
+public class ReservationService
+{
+    private readonly IMongoCollection<Reservation> _reservations;
+
+    public ReservationService(IMongoClient client, IOptions<MongoDbSettings> settings)
+    {
+        var db = client.GetDatabase(settings.Value.DatabaseName);
+        _reservations = db.GetCollection<Reservation>("Reservations");
+    }
+
+    public List<Reservation> GetAll() =>
+        _reservations.Find(_ => true).ToList();
+
+    public Reservation GetById(string id) =>
+        _reservations.Find(r => r.Id == id).FirstOrDefault();
+
+    public List<Reservation> GetByGuest(string guestId) =>
+        _reservations.Find(r => r.GuestId == guestId)
+                     .SortByDescending(r => r.CreatedAt)
+                     .ToList();
+
+    public List<Reservation> GetByAccommodation(string accommodationId) =>
+        _reservations.Find(r => r.AccommodationId == accommodationId)
+                     .SortBy(r => r.CheckIn)
+                     .ToList();
+
+    public void Delete(string id) =>
+        _reservations.DeleteOne(r => r.Id == id);
+}
